fcea9b818c4d8ed15c85d244ef87e0d7a98d747e
Sentry2703 <adetunjiadewoye1@gmail.com>
2023-12-30 00:44:44 -0500

    Added Form for Meta Course

--- demoproj/demoapp/urls.py
+++ demoproj/demoapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = "index"),
     path('home/', views.home, name = "home"),
+    path('form/', views.form, name = "form"),
+    path('getform/', views.form_submit, name = "getform")
 ]
--- demoproj/demoapp/views.py
+++ demoproj/demoapp/views.py
@@ -2,8 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the indx of Demo Project.")
+    return HttpResponse("Hello, world. You're at the index of Demo Project.")
 
 def home(request):
-    return HttpResponse("Welcome to the Little Lemon Website")
-# Create your views here.
+
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+
+    msg = """
+            <h1>Welcome to the Little Lemon Website</h1>
+            <br>
+            <br>Path: {}
+            <br>Scheme: {}
+            <br>Method: {}
+            <br>Address: {}
+            <br>User Agent: {}
+            <br>Path Info: {}""".format(path, scheme, method, address, user_agent, path_info)
+
+    return HttpResponse(msg, content_type = "text/html", charset = "utf-8")
+
+def form(request):
+    return render(request, 'demoapp/form.html')
+
+def form_submit(request):
+    if request.method == "POST":
+        username = request.POST.get('name')
+        age = request.POST.get('age')
+        return HttpResponse("Username: {}, Password: {}".format(username, age))
+    else:
+        return HttpResponse("Invalid request method.")
