0ea24cbe88ce759d19a953235264e2fce0a23dac
Sentry2703 <adetunjiadewoye1@gmail.com>
2023-12-31 23:07:44 -0500

    Self Project: Admin Model registered

--- statesAir/airport/admin.py
+++ statesAir/airport/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Plane, Flight, Department, Employee, Crew
 
 # Register your models here.
+admin.site.site_header = "StatesAir Administration"
+admin.site.register(Plane)
+admin.site.register(Flight)
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Crew)
--- statesAir/airport/urls.py
+++ statesAir/airport/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = 'airport'
+
 urlpatterns = [
-    path('', views.home, name = "airport-home"),
-    path('form/', views.form, name = "airport-form"),
-    path('create_dep/', views.create_dep, name = "airport-create_dep"),
+    path('', views.home, name = "home"),
+    path('form/', views.form, name = "form"),
+    path('create_dep/', views.create_dep, name = "create_dep"),
 ]
