7d34164000f14c9cbdaedf12ce627bb51bcb3600
Sentry2703 <adetunjiadewoye1@gmail.com>
2023-12-31 22:05:13 -0500

    Self Project: Added views and html templates for Data Entry

--- statesAir/airport/urls.py
+++ statesAir/airport/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name = "airport-home")
+    path('', views.home, name = "airport-home"),
+    path('form/', views.form, name = "airport-form"),
+    path('create_dep/', views.create_dep, name = "airport-create_dep"),
 ]
--- statesAir/airport/views.py
+++ statesAir/airport/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import DepartmentForm, CreateDepartmentForm
 
 def home(request):
     return HttpResponse("This is the Airport Application")
+
+def form(request):
+    form = DepartmentForm()
+    context = {"form" : form}
+    return render(request, 'airport/form.html', context)
+
+def create_dep(request):
+    form = CreateDepartmentForm()
+    if request.method == "POST":
+        form = CreateDepartmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form" : form}
+    return render(request, 'airport/create_dep.html', context)
