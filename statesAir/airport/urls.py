2d8d7b968b682bbc9d64b361dd73bb92214fabe4
Sentry2703 <adetunjiadewoye1@gmail.com>
2024-01-03 22:54:23 -0500

    Self Project: Added new urls and applicable views to the project

--- statesAir/airport/urls.py
+++ statesAir/airport/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.home, name = "home"),
     path('create_dep/', views.create_dep, name = "create_dep"),
     path('list_things/', views.list_things, name = "list_things"),
+    path('create_emp/', views.create_emp, name = "create_emp"),
+    path('create_plane/', views.create_plane, name = "create_plane"),
+    path('create_flight/', views.create_flight, name = "create_flight"),
 ]
--- statesAir/airport/views.py
+++ statesAir/airport/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import DepartmentForm, CreateDepartmentForm
+from .forms import CreateDepartmentForm, CreateEmployeeForm, CreateFlightForm, CreatePlaneForm
 from .models import Employee, Department, Plane, Flight, Crew
 
 def home(request):
@@ -14,6 +14,33 @@ def create_dep(request):
             form.save()
     context = {"form" : form}
     return render(request, 'airport/create_dep.html', context)
+
+def create_emp(request):
+    form = CreateEmployeeForm()
+    if request.method == "POST":
+        form = CreateEmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form" : form}
+    return render(request, 'airport/create_emp.html', context)
+
+def create_plane(request):
+    form = CreatePlaneForm()
+    if request.method == "POST":
+        form = CreatePlaneForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form" : form}
+    return render(request, 'airport/create_plane.html', context)
+
+def create_flight(request):
+    form = CreateFlightForm()
+    if request.method == "POST":
+        form = CreateFlightForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form" : form}
+    return render(request, 'airport/create_flight.html', context)
 
 def list_things(request):
     emp = Employee.objects.all()
