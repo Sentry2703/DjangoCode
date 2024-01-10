c3e10d42e548de10c3b186d6bed07a9525ea6087
Sentry2703 <adetunjiadewoye1@gmail.com>
2024-01-10 09:55:03 -0500

    Course Correct

--- BookList/BookList/urls.py
+++ BookList/BookList/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Books project.
+URL configuration for BookList project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
